08e3160cf6a4ca42bfc27a9da22a14e842c5ab99
Meurig Thomas <mlt47@cam.ac.uk>
2019-08-06 17:06:10 +0100

    Move to using SUBJECT_INTEREST rather than KPI_DEMOGRAPHIC

--- src/main/java/uk/ac/cam/cl/dtg/isaac/api/Constants.java
+++ src/main/java/uk/ac/cam/cl/dtg/isaac/api/Constants.java
@@ -133,7 +133,7 @@ public final class Constants {
     public static final Set<String> ISAAC_LOG_TYPES = Arrays.stream(IsaacLogType.values()).map(IsaacLogType::name).collect(Collectors.toSet());
 
     public enum IsaacUserPreferences {
-        SUBJECT_INTEREST, BETA_FEATURE, EXAM_BOARD, KPI_DEMOGRAPHIC
+        SUBJECT_INTEREST, BETA_FEATURE, EXAM_BOARD
     }
 
     /**
